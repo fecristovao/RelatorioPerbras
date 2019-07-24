52d5738f220914daefd83a11fc5eef3a23f26483
Fernando Cristovao <fernandocristovao@id.uff.br>
2019-07-24 12:38:52 +0000

    Adicionado Materialize e CSS

--- src/router.js
+++ src/router.js
@@ -1,31 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Espelho from './views/Espelho.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: function () { 
-        return import(/* webpackChunkName: "about" */ './views/About.vue')
-      }
-    },
-    {
-      path: '/id/:id/contrato/:contrato',
-      name: 'zuado',
-      component: Home,
-      props: true
+      path: '/espelho/:id/:contrato',
+      name: 'espelho',
+      component: Espelho
     }
   ]
 })
